10c3927e399e4e015c1f1914d74236f9f91ee2a5
Manav Kataria <manavkataria.iitb@gmail.com>
2016-11-12 18:32:11 -0800

    Output formatting; Final Moments

--- run_dataset.py
+++ run_dataset.py
@@ -43,15 +43,16 @@ while((cv2.waitKey(10) & 0xFF) != ord('q')):
         image = scipy.misc.imresize(full_image[-150:], [66, 200]) / 255.0
         degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / scipy.pi
         # call("clear")
-        print "Frame [%5d => %2d%%]: Predicted steering angle: %3.0f degrees" % (i, i*100.0/total_files, degrees)
-        cv2.imshow("frame", cv2.cvtColor(full_image, cv2.COLOR_RGB2BGR))
+        # print "Frame [%5d => %2d%%]: Predicted steering angle: %3.0f degrees" % (i, i*100.0/total_files, degrees)
+        print "%s, %s" % (frame, degrees/180*8.2)
+        # cv2.imshow("frame", cv2.cvtColor(full_image, cv2.COLOR_RGB2BGR))
         #make smooth angle transitions by turning the steering wheel based on the difference of the current angle
         #and the predicted angle
         smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
-        M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
-        dst = cv2.warpAffine(img,M,(cols,rows))
-        cv2.imshow("steering wheel", dst)
-        cv2.waitKey(1)
+        # M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
+        # dst = cv2.warpAffine(img,M,(cols,rows))
+        # cv2.imshow("steering wheel", dst)
+        # cv2.waitKey(1)
 
 cv2.destroyAllWindows()
 
